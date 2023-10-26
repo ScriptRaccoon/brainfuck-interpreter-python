958771e23eafd0ce370bf511e60fbe7fd95fcd99
Script Raccoon <scriptraccoon@gmail.com>
2023-10-26 22:04:16 +0200

    conversion to class

--- brainfuck.py
+++ brainfuck.py
@@ -3,150 +3,189 @@ Brainfuck interpreter written in Python
 https://en.wikipedia.org/wiki/Brainfuck
 
 Usage:
-python brainfuck.py [file path]
+python brainfuck.py example1.bf
 or
-python brainfuck.py [file path] debug
+python brainfuck.py example1.bf debug
 """
-
 
 import sys
 
-ALLOWED_CHARS = list("+-><[].,")
 
-
-def generate_bracket_dict(program: str) -> dict[int, int]:
+class BrainfuckInterpreter:
     """
-    Generates a dictionary of bracket infos for a brainfuck program.
-    When a bracket [ starts at position i and ends with ] at position j,
-    the dictionary has i: j as well as j: i.
+    Creates a Brainfuck interpreter for a given Brainfuck program
 
     Arguments:
-        program: any valid Brainfuck program
+        program: any Brainfuck program (as a string)
+        debug (optional): if True, the interpreter displays the commands and the tape in each step
 
-    Returns:
-        The bracket dictionary
-
-    Raises:
-        SyntaxError: When the brackets do not match up
+    Attributes:
+        program: the Brainfuck program
+        debug: the flag determining if the interpreter prints debug info
+        bracket_dict: a dictionary containing matching bracket positions
+        pos: current position of the program during interpretation
+        tape: the array of integers on which the program operates
+        cell: the index of the current cell on the tape
     """
-    bracket_dict: dict[int, int] = {}
-    opening_bracket_positions = []
-    for pos, char in enumerate(program):
-        if char == "[":
-            opening_bracket_positions.append(pos)
-        elif char == "]":
-            if len(opening_bracket_positions) == 0:
-                raise SyntaxError(f"] in position {pos} has no matching [")
-            opening_bracket = opening_bracket_positions.pop()
-            bracket_dict[opening_bracket] = pos
-            bracket_dict[pos] = opening_bracket
-    if len(opening_bracket_positions) > 0:
-        pos = opening_bracket_positions[0]
-        raise SyntaxError(f"[ in position {pos} has no matching ]")
-    return bracket_dict
 
+    ALLOWED_CHARS = list("+-><[].,")
+    """List of allowed characters in a Brainfuck program, all others are just ignored."""
 
-def interpret(program: str, debug=False) -> None:
-    """
-    Python interpreter of a brainfuck program.
-    It keeps track of
-        - a pointer to the current position of the program
-        - the index of the current cell
-        - the tape, encoded as a list of integers
+    def __init__(self, program: str, debug: bool = False) -> None:
+        """
+        Initializes the Brainfuck interpreter and generates the bracket dictionary
 
-    Arguments:
-        program: any valid Brainfuck program as a string
-        debug (optional): if True, prints the current state at each step
+        Arguments:
+            program: any Brainfuck program (as a string)
+            debug (optional): if True, interpreter displays commands and tape in each step
+        """
+        self.program = program
+        self.debug = debug
+        self.bracket_dict = self.generate_bracket_dict()
+        self.pos: int = 0
+        self.tape: list[int] = [0]
+        self.cell: int = 0
 
-    Raises:
-        IndexError: when the tape index is negative
-    """
-    if debug:
-        print("program:", program)
+    def increment(self) -> None:
+        """Increments the current cell value (restricted to 1 byte)"""
+        self.tape[self.cell] += 1
+        if self.tape[self.cell] > 255:
+            self.tape[self.cell] = 0
 
-    pointer: int = 0
-    cell: int = 0
-    tape: list[int] = [0]
+    def decrement(self) -> None:
+        """Decrements the current cell value (restricted to 1 byte)"""
+        self.tape[self.cell] -= 1
+        if self.tape[self.cell] < 0:
+            self.tape[self.cell] = 255
 
-    bracket_dict = generate_bracket_dict(program)
+    def go_right(self) -> None:
+        """Go right on the tape"""
+        self.cell += 1
+        if self.cell >= len(self.tape):
+            self.tape.append(0)
 
-    while pointer < len(program):
-        char: str = program[pointer]
+    def go_left(self) -> None:
+        """Go left on the tape (if possible)"""
+        if self.cell == 0:
+            raise IndexError("Tape does not have negative indices")
+        self.cell -= 1
 
-        if not char in ALLOWED_CHARS:
-            pointer += 1
-            continue
+    def start_loop(self) -> None:
+        """Start the loop: either skip it (when the call value is 0) or enter it"""
+        if self.tape[self.cell] == 0:
+            if self.debug:
+                print("skip loop")
+            self.pos = self.bracket_dict[self.pos]
+        elif self.debug:
+            print("enter loop")
 
-        if debug:
-            tape_with_pos = ", ".join(
-                str(val) if i != cell else f"|{str(val)}|" for i, val in enumerate(tape)
-            )
-            print("tape:", tape_with_pos)
-            print("char:", char)
+    def finish_loop(self) -> None:
+        """Finish the loop: either finish it (when the cell value is 0) or restart it over"""
+        if self.tape[self.cell] != 0:
+            if self.debug:
+                print("restart loop")
+            self.pos = self.bracket_dict[self.pos]
+        elif self.debug:
+            print("finish loop")
 
-        if char == "+":
-            tape[cell] += 1
-            if tape[cell] > 255:
-                tape[cell] = 0
+    def print(self) -> None:
+        """Prints the current cell value as an ascii character"""
+        ascii_index = self.tape[self.cell]
+        ascii_char = chr(ascii_index)
+        if self.debug:
+            print("print:", ascii_char)
+        else:
+            print(ascii_char, end="")
 
-        elif char == "-":
-            tape[cell] -= 1
-            if tape[cell] < 0:
-                tape[cell] = 255
+    def read(self) -> None:
+        """
+        Reads one ascii character from standard input and
+        puts its ascii index on the current cell
+        """
+        ascii_char = ""
+        valid = False
+        while not valid:
+            ascii_char = input()
+            valid = len(ascii_char) == 1 and 0 <= ord(ascii_char) <= 255
+            if not valid:
+                print("Error: Expected only one ascii character as input. Try again.")
+        ascii_index = ord(ascii_char)
+        self.tape[self.cell] = ascii_index
 
-        elif char == ">":
-            cell += 1
-            if cell >= len(tape):
-                tape.append(0)
+    def generate_bracket_dict(self) -> dict[int, int]:
+        """
+        Generates a dictionary of bracket positions for the brainfuck program.
+        When a bracket [ starts at position i and ends with ] at position j,
+        the dictionary has i -> j as well as j -> i. This facilitates jumping
+        between the bracket positions when running the interpreter.
 
-        elif char == "<":
-            if cell == 0:
-                raise IndexError("Tape does not have negative indices")
-            cell -= 1
+        Returns:
+            The bracket dictionary
 
-        elif char == "[":
-            if tape[cell] == 0:
-                if debug:
-                    print("skip loop")
-                pointer = bracket_dict[pointer]
-            elif debug:
-                print("enter loop")
+        Raises:
+            SyntaxError: When the brackets do not match up
+        """
+        bracket_dict: dict[int, int] = {}
+        opening_bracket_positions = []
+        for pos, char in enumerate(self.program):
+            if char == "[":
+                opening_bracket_positions.append(pos)
+            elif char == "]":
+                if len(opening_bracket_positions) == 0:
+                    raise SyntaxError(f"] in position {pos} has no matching [")
+                opening_bracket = opening_bracket_positions.pop()
+                bracket_dict[opening_bracket] = pos
+                bracket_dict[pos] = opening_bracket
+        if len(opening_bracket_positions) > 0:
+            pos = opening_bracket_positions[0]
+            raise SyntaxError(f"[ in position {pos} has no matching ]")
 
-        elif char == "]":
-            if tape[cell] != 0:
-                if debug:
-                    print("restart loop")
-                pointer = bracket_dict[pointer]
-            elif debug:
-                print("finish loop")
+        return bracket_dict
 
-        elif char == ".":
-            ascii_value = tape[cell]
-            string_value = chr(ascii_value)
-            if debug:
-                print("print:", string_value)
-            else:
-                print(string_value, end="")
+    def run(self) -> None:
+        """Runs the Brainfuck interpreter"""
+        if self.debug:
+            print("program:", self.program)
 
-        elif char == ",":
-            string_value = ""
-            valid = False
-            while not valid:
-                string_value = input()
-                valid = len(string_value) == 1 and 0 <= ord(string_value) <= 255
-                if not valid:
-                    print(
-                        "Error: Expected only one ascii character as input. Try again."
-                    )
-            ascii_value = ord(string_value)
-            tape[cell] = ascii_value
+        while self.pos < len(self.program):
+            char: str = self.program[self.pos]
 
-        pointer += 1
+            if not char in BrainfuckInterpreter.ALLOWED_CHARS:
+                self.pos += 1
+                continue
+
+            if self.debug:
+                tape_with_pos = ", ".join(
+                    str(val) if i != self.cell else f"|{str(val)}|"
+                    for i, val in enumerate(self.tape)
+                )
+                print("tape:", tape_with_pos)
+                print("char:", char)
+
+            if char == "+":
+                self.increment()
+            elif char == "-":
+                self.decrement()
+            elif char == ">":
+                self.go_right()
+            elif char == "<":
+                self.go_left()
+            elif char == "[":
+                self.start_loop()
+            elif char == "]":
+                self.finish_loop()
+            elif char == ".":
+                self.print()
+            elif char == ",":
+                self.read()
+
+            self.pos += 1
 
 
 def main() -> None:
     """
-    Executes the interpreter with a file passed as an argument
+    Reads the file passed as an argument, creates a brainfuck
+    interpreter for it and executes it.
     """
     args = sys.argv
     if len(args) == 1:
@@ -155,7 +194,8 @@ def main() -> None:
     debug = len(args) == 3 and args[2] == "debug"
     with open(file_path, "r", encoding="utf8") as file:
         program = file.read()
-        interpret(program, debug)
+        interpreter = BrainfuckInterpreter(program, debug)
+        interpreter.run()
 
 
 if __name__ == "__main__":
